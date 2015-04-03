d43688b86a22ece958ac3fe479602ceed4032bcb
Steinar Bang <steb@soprasteria.com>
2015-04-03 15:42:34 +0200

    Find all classes of a bundle.

--- jsr330activator.implementation/src/main/java/no/steria/osgi/jsr330activator/Jsr330Activator.java
+++ jsr330activator.implementation/src/main/java/no/steria/osgi/jsr330activator/Jsr330Activator.java
@@ -1,10 +1,16 @@
 package no.steria.osgi.jsr330activator;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Provider;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.wiring.BundleWiring;
 
 /**
  * An OSGi {@link BundleActivator} implementation, that will scan the
@@ -36,6 +42,29 @@ public class Jsr330Activator implements BundleActivator {
 
     public void stop(BundleContext context) throws Exception {
         // TODO add deactivation code here
+    }
+
+    List<Class<?>> scanBundleForClasses(Bundle bundle) {
+    	List<Class<?>> classes = new ArrayList<Class<?>>();
+    	try {
+            BundleWiring bundleWiring = bundle.adapt(BundleWiring.class);
+            if (null != bundleWiring) {
+                Collection<String> classnames = bundleWiring.listResources("/", "*", BundleWiring.LISTRESOURCES_LOCAL);
+                if (null != classnames) {
+                    ClassLoader bundleClassLoader = bundleWiring.getClassLoader();
+                    if (null != bundleClassLoader) {
+                    	for (String classname : classnames) {
+                            try {
+                                Class<?> clazz = bundleClassLoader.loadClass(classname);
+                                classes.add(clazz);
+                            } catch (ClassNotFoundException e) { }
+                        }
+                    }
+                }
+            }
+    	} catch (SecurityException e) { }
+
+        return classes;
     }
 
 }
--- jsr330activator.implementation/src/test/java/no/steria/osgi/jsr330activator/Jsr330ActivatorTest.java
+++ jsr330activator.implementation/src/test/java/no/steria/osgi/jsr330activator/Jsr330ActivatorTest.java
@@ -1,9 +1,15 @@
 package no.steria.osgi.jsr330activator;
 
+import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.wiring.BundleWiring;
 
 public class Jsr330ActivatorTest {
 
@@ -11,9 +17,102 @@ public class Jsr330ActivatorTest {
     public void setUp() throws Exception {
     }
 
+    /**
+     * Unit test for {@link Jsr330Activator#scanBundleForClasses(Bundle)}.
+     */
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testScanBundleForClasses() {
+    	Bundle bundle = mock(Bundle.class);
+    	BundleWiring bundleWiring = mock(BundleWiring.class);
+    	// Names of 3 classes found in the test project, name of 1 class not found (to test the try/catch)
+    	List<String> classnames = Arrays.asList("no.steria.osgi.jsr330activator.testbundle.HelloService", "no.steria.osgi.jsr330activator.testbundle.implementation.HelloServiceImplementation", "no.steria.osgi.jsr330activator.testbundle.implementation.HelloServiceProvider", "no.steria.osgi.jsr330activator.testbundle.implementation.NotFoundClass");
+    	when(bundleWiring.listResources(anyString(), anyString(), eq(BundleWiring.LISTRESOURCES_LOCAL))).thenReturn(classnames);
+    	when(bundleWiring.getClassLoader()).thenReturn(this.getClass().getClassLoader());
+    	when(bundle.adapt(eq(BundleWiring.class))).thenReturn(bundleWiring);
+
+    	Jsr330Activator activator = new Jsr330Activator();
+        List<Class<?>> bundleClasses = activator.scanBundleForClasses(bundle);
+        assertEquals(3, bundleClasses.size());
+    }
+
+    /**
+     * Corner case unit test for {@link Jsr330Activator#scanBundleForClasses(Bundle)}.
+     * Test the case where {@link Bundle#adapt(Class)} returns null for a {@link BundleWiring} class.
+     */
+    @Test
+    public void testScanBundleForClassesNullBundleWiringFromAdapt() {
+    	Bundle bundle = mock(Bundle.class);
+
+    	Jsr330Activator activator = new Jsr330Activator();
+        List<Class<?>> bundleClasses = activator.scanBundleForClasses(bundle);
+        assertEquals(0, bundleClasses.size());
+    }
+
+    /**
+     * Corner case unit test for {@link Jsr330Activator#scanBundleForClasses(Bundle)}.
+     * Test the case where {@link Bundle#adapt(Class)} for a {@link BundleWiring} class throws a {@link SecurityException}.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testScanBundleForClassesBundleWiringAdaptThrowsSecurityException() {
+    	Bundle bundle = mock(Bundle.class);
+    	when(bundle.adapt(eq(BundleWiring.class))).thenThrow(SecurityException.class);
+
+    	Jsr330Activator activator = new Jsr330Activator();
+        List<Class<?>> bundleClasses = activator.scanBundleForClasses(bundle);
+        assertEquals(0, bundleClasses.size());
+    }
+
+    /**
+     * Corner case unit test for {@link Jsr330Activator#scanBundleForClasses(Bundle)}.
+     * Test the case where {@link BundleWiring#getClassLoader()} returns null.
+     */
+    @Test
+    public void testScanBundleForClassesNullClassloaderFromBundleWiring() {
+    	Bundle bundle = mock(Bundle.class);
+    	BundleWiring bundleWiring = mock(BundleWiring.class);
+    	List<String> classnames = Arrays.asList("no.steria.osgi.jsr330activator.testbundle.HelloService", "no.steria.osgi.jsr330activator.testbundle.implementation.HelloServiceImplementation", "no.steria.osgi.jsr330activator.testbundle.implementation.HelloServiceProvider", "no.steria.osgi.jsr330activator.testbundle.implementation.NotFoundClass");
+    	when(bundleWiring.listResources(anyString(), anyString(), eq(BundleWiring.LISTRESOURCES_LOCAL))).thenReturn(classnames);
+    	when(bundle.adapt(eq(BundleWiring.class))).thenReturn(bundleWiring);
+
+    	Jsr330Activator activator = new Jsr330Activator();
+        List<Class<?>> bundleClasses = activator.scanBundleForClasses(bundle);
+        assertEquals(0, bundleClasses.size());
+    }
+
+    /**
+     * Corner case unit test for {@link Jsr330Activator#scanBundleForClasses(Bundle)}.
+     * Test the case where {@link BundleWiring#getClassLoader()} throws a {@link SecurityException}.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testScanBundleForClassesBundleWiringGetClassLoaderThrowsSecurityException() {
+    	Bundle bundle = mock(Bundle.class);
+    	BundleWiring bundleWiring = mock(BundleWiring.class);
+    	List<String> classnames = Arrays.asList("no.steria.osgi.jsr330activator.testbundle.HelloService", "no.steria.osgi.jsr330activator.testbundle.implementation.HelloServiceImplementation", "no.steria.osgi.jsr330activator.testbundle.implementation.HelloServiceProvider", "no.steria.osgi.jsr330activator.testbundle.implementation.NotFoundClass");
+    	when(bundleWiring.listResources(anyString(), anyString(), eq(BundleWiring.LISTRESOURCES_LOCAL))).thenReturn(classnames);
+    	when(bundleWiring.getClassLoader()).thenThrow(SecurityException.class);
+    	when(bundle.adapt(eq(BundleWiring.class))).thenReturn(bundleWiring);
+
+    	Jsr330Activator activator = new Jsr330Activator();
+        List<Class<?>> bundleClasses = activator.scanBundleForClasses(bundle);
+        assertEquals(0, bundleClasses.size());
+    }
+
+    /**
+     * Corner case unit test for {@link Jsr330Activator#scanBundleForClasses(Bundle)}.
+     * Test the case where {@link BundleWiring#listResources(String, String, int)} returns a null (for a BundleWiring not in use).
+     */
+    @Test
+    public void testScanBundleForClassesListResourcesReturnsNull() {
+    	Bundle bundle = mock(Bundle.class);
+    	BundleWiring bundleWiring = mock(BundleWiring.class);
+    	when(bundleWiring.listResources(anyString(), anyString(), eq(BundleWiring.LISTRESOURCES_LOCAL))).thenReturn(null);
+    	when(bundle.adapt(eq(BundleWiring.class))).thenReturn(bundleWiring);
+
+    	Jsr330Activator activator = new Jsr330Activator();
+        List<Class<?>> bundleClasses = activator.scanBundleForClasses(bundle);
+        assertEquals(0, bundleClasses.size());
     }
 
 }
