e297ab8bfb132c90a8cff50de355bc6073df153d
Steinar Bang <steb@soprasteria.com>
2015-04-06 07:49:10 +0200

    Removed unused import org.ops4j.pax.exam.util.PathUtils
    This import is needed if the Pax Exam OSGi runtime is to use a
    dependency from a directory (needs a manifest in the right place, and
    m2e with maven-bundle-plugin can't build manifest files)

--- jsr330activator.tests/src/test/java/no/steria/osgi/jsr330activator/tests/Jsr330ActivatorIntegrationTest.java
+++ jsr330activator.tests/src/test/java/no/steria/osgi/jsr330activator/tests/Jsr330ActivatorIntegrationTest.java
@@ -19,7 +19,6 @@ import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
-import org.ops4j.pax.exam.util.PathUtils;
 import org.osgi.framework.BundleContext;
 
 @RunWith(PaxExam.class)
