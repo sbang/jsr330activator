596017f8514b550dc3ac891d62fd0fc6227b81fb
Steinar Bang <steb@soprasteria.com>
2015-04-18 17:59:56 +0200

    Added simple mocking of service registration notification, and service registration retrieval.

--- jsr330activator.mocks/src/main/java/no/steria/osgi/mocks/MockBundleContext.java
+++ jsr330activator.mocks/src/main/java/no/steria/osgi/mocks/MockBundleContext.java
@@ -9,6 +9,9 @@ import java.util.Map.Entry;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 
@@ -19,9 +22,25 @@ import org.osgi.framework.ServiceRegistration;
  *
  */
 public class MockBundleContext extends MockBundleContextBase {
+    @Override
+    public ServiceReference<?>[] getServiceReferences(String clazz, String filter) throws InvalidSyntaxException {
+        // Note: neither the clazz nor the filter argument is handled in any way
+    	// If this is needed to make tests run as expected it must be implemented
+    	// For now, just return all of the references in the mock.
+    	ServiceReference<?>[] serviceReferences = new ServiceReference<?>[serviceRegistrations.size()];
+    	int i = 0;
+    	for (Entry<String, ServiceReference<?>> serviceRegistration : serviceRegistrations.entrySet()) {
+            serviceReferences[i] = serviceRegistration.getValue();
+            ++i;
+        }
+
+    	return serviceReferences;
+    }
+
     MockBundle bundle;
     Map<String, ServiceReference<?>> serviceRegistrations = new HashMap<String, ServiceReference<?>>();
     Map<ServiceReference<?>, Object> serviceImplementations = new HashMap<ServiceReference<?>, Object>();
+    Map<String, List<ServiceListener>> filteredServiceListeners = new HashMap<String, List<ServiceListener>>();
 
     public MockBundleContext(MockBundle bundle) {
         this.bundle = bundle;
@@ -41,6 +60,7 @@ public class MockBundleContext extends MockBundleContextBase {
         MockServiceReference<Object> serviceReference = new MockServiceReference<Object>(getBundle());
         serviceRegistrations.put(clazz, serviceReference);
         serviceImplementations.put(serviceReference, service);
+        notifyListenersAboutNewService(clazz, serviceReference);
         return new MockServiceRegistration<Object>(this, serviceReference);
     }
 
@@ -80,6 +100,26 @@ public class MockBundleContext extends MockBundleContextBase {
         }
 
         return unregistrationSuccess;
+    }
+
+    @Override
+    public void addServiceListener(ServiceListener listener, String filter) throws InvalidSyntaxException {
+        if (null == filteredServiceListeners.get(filter)) {
+            filteredServiceListeners.put(filter, new ArrayList<ServiceListener>(1));
+        }
+
+        filteredServiceListeners.get(filter).add(listener);
+    }
+
+    private void notifyListenersAboutNewService(String clazz, MockServiceReference<Object> serviceReference) {
+        String filter = "(objectclass=" + clazz + ")";
+        List<ServiceListener> filteredServiceListenerList = filteredServiceListeners.get(filter);
+        if (null != filteredServiceListenerList) {
+            ServiceEvent newServiceEvent = new ServiceEvent(ServiceEvent.REGISTERED, serviceReference);
+            for (ServiceListener serviceListener : filteredServiceListenerList) {
+                serviceListener.serviceChanged(newServiceEvent);
+            }
+        }
     }
 
 }
