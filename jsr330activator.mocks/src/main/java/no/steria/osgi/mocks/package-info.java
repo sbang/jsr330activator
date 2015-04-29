6f8d519e5af3944d84cd92be955aecce44224987
Steinar Bang <steb@soprasteria.com>
2015-04-29 22:17:03 +0200

    Added Javadoc package information to the OSGi mocks project.

---
+++ jsr330activator.mocks/src/main/java/no/steria/osgi/mocks/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * This package contains mock implementations of the <a href="http://www.osgi.org/">OSGi framework</a>
+ * similar to what's found in <a href="https://sling.apache.org/documentation/development/osgi-mock.html">Apache Sling OSGi mocks</a>
+ * and in <a href="http://docs.spring.io/osgi/docs/current/reference/html/testing.html">Spring OSGi mocks</a>.
+ *
+ * <p>The implemented classes implement basic service registration, and retrieval,
+ * service notification and callback registration.
+ */
+/**
+ * @author Steinar Bang
+ *
+ */
+package no.steria.osgi.mocks;
