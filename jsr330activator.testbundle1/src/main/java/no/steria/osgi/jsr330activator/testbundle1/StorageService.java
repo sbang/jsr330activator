d6ae3bd490f5ce86f31aecea313448957c2f8420
Steinar Bang <steb@soprasteria.com>
2015-05-07 20:20:43 +0200

    Added the StorageService service interface.
    Residing in a package that is exported from an OSGi package.

--- jsr330activator.testbundle1/src/main/java/no/steria/osgi/jsr330activator/testbundle1/StorageService.java
+++ jsr330activator.testbundle1/src/main/java/no/steria/osgi/jsr330activator/testbundle1/StorageService.java
@@ -1,5 +1,8 @@
 package no.steria.osgi.jsr330activator.testbundle1;
 
-public interface HelloService {
-    public String getMessage();
+import java.util.UUID;
+
+public interface StorageService {
+    public boolean save(UUID id, String data);
+    String load(UUID id);
 }
