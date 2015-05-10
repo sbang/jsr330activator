2eede4abb43f8aff4145772a452cc2b11c3275d9
Steinar Bang <steb@soprasteria.com>
2015-05-10 22:56:27 +0200

    Made test bundle service providers actually instantiate a service.
    Return "this" instead of "null" from the providers that will catch
    multiple instances of the StorageService in the integration test.

--- jsr330activator.testbundle7/src/main/java/no/steria/osgi/jsr330activator/testbundle7/implementation/CollectionInjectionCatcherProvider.java
+++ jsr330activator.testbundle7/src/main/java/no/steria/osgi/jsr330activator/testbundle7/implementation/CollectionInjectionCatcherProvider.java
@@ -6,14 +6,13 @@ import no.steria.osgi.jsr330activator.testbundle7.CollectionInjectionCatcher;
 
 public class CollectionInjectionCatcherProvider implements Provider<CollectionInjectionCatcherProvider>, CollectionInjectionCatcher {
 
-	public String getMessage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public String getMessage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	public CollectionInjectionCatcherProvider get() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public CollectionInjectionCatcherProvider get() {
+        return this;
+    }
 
 }
--- jsr330activator.testbundle7/src/main/java/no/steria/osgi/jsr330activator/testbundle7/implementation/OptionalInjectionCatcherProvider.java
+++ jsr330activator.testbundle7/src/main/java/no/steria/osgi/jsr330activator/testbundle7/implementation/OptionalInjectionCatcherProvider.java
@@ -6,14 +6,13 @@ import no.steria.osgi.jsr330activator.testbundle7.OptionalInjectionCatcher;
 
 public class OptionalInjectionCatcherProvider implements Provider<OptionalInjectionCatcher>, OptionalInjectionCatcher {
 
-	public OptionalInjectionCatcher get() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public String getMessage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	public String getMessage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public OptionalInjectionCatcher get() {
+        return this;
+    }
 
 }
