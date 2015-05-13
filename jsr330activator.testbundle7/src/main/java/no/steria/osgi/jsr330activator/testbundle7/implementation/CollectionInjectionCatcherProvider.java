d86d11262f1915dc09370ffe2adb6c536c7ad552
Steinar Bang <steb@soprasteria.com>
2015-05-13 22:22:52 +0200

    Added StorageService list injection to the CollectionInjectionCatcherProvider.
    Also implemented the new CollectionInjectionCatcher methods, with the
    getNumberOfInjectedStorageServices() method returning the number of
    injected StorageService instances.

--- jsr330activator.testbundle7/src/main/java/no/steria/osgi/jsr330activator/testbundle7/implementation/CollectionInjectionCatcherProvider.java
+++ jsr330activator.testbundle7/src/main/java/no/steria/osgi/jsr330activator/testbundle7/implementation/CollectionInjectionCatcherProvider.java
@@ -1,18 +1,34 @@
 package no.steria.osgi.jsr330activator.testbundle7.implementation;
 
+import java.util.Collection;
+import java.util.List;
+
+import javax.inject.Inject;
 import javax.inject.Provider;
 
 import no.steria.osgi.jsr330activator.testbundle7.CollectionInjectionCatcher;
+import no.steria.osgi.jsr330activator.testbundle8.StorageService;
 
 public class CollectionInjectionCatcherProvider implements Provider<CollectionInjectionCatcher>, CollectionInjectionCatcher {
+    @Inject
+    List<StorageService> storageServices;
 
-    public String getMessage() {
+    public CollectionInjectionCatcherProvider get() {
+        return this;
+    }
+
+    public int getNumberOfInjectedStorageServices() {
+        return storageServices.size();
+    }
+
+    public Collection<String> getInjectedStorageServiceNames() {
         // TODO Auto-generated method stub
         return null;
     }
 
-    public CollectionInjectionCatcherProvider get() {
-        return this;
+    public StorageService getStorageService(String name) {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
