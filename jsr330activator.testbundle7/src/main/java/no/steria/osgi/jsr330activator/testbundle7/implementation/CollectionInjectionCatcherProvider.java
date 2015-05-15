e186ae7e94738bab8a91117c2e1ca0c9dd29cfb2
Steinar Bang <steb@soprasteria.com>
2015-05-15 21:01:34 +0200

    Implemented the CollectionInjectionCatcherProvider getInjectedStorageServiceNames() and getStorageService() methods.
    Needed to access the @Named annotations on the providers, which showed
    up as dynamic proxies, and to make the dynamic proxies work (make
    instanceof and casting work), the javax.inject annotations needed to be
    loaded by the same classloader, which in turn meant that javax.inject
    had to be loaded from the same bundle by all of the involved
    testbundles (testbundle4, 5, 6 and 7).

--- jsr330activator.testbundle7/src/main/java/no/steria/osgi/jsr330activator/testbundle7/implementation/CollectionInjectionCatcherProvider.java
+++ jsr330activator.testbundle7/src/main/java/no/steria/osgi/jsr330activator/testbundle7/implementation/CollectionInjectionCatcherProvider.java
@@ -1,9 +1,13 @@
 package no.steria.osgi.jsr330activator.testbundle7.implementation;
 
+import java.lang.annotation.Annotation;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.inject.Provider;
 
 import no.steria.osgi.jsr330activator.testbundle7.CollectionInjectionCatcher;
@@ -12,6 +16,8 @@ import no.steria.osgi.jsr330activator.testbundle8.StorageService;
 public class CollectionInjectionCatcherProvider implements Provider<CollectionInjectionCatcher>, CollectionInjectionCatcher {
     @Inject
     List<StorageService> storageServices;
+
+    Map<String, StorageService> serviceNameMap = new HashMap<String, StorageService>();
 
     public CollectionInjectionCatcherProvider get() {
         return this;
@@ -22,12 +28,35 @@ public class CollectionInjectionCatcherProvider implements Provider<CollectionIn
     }
 
     public Collection<String> getInjectedStorageServiceNames() {
-        // TODO Auto-generated method stub
-        return null;
+    	updateServiceNameMapIfNeeded();
+        return serviceNameMap.keySet();
     }
 
     public StorageService getStorageService(String name) {
-        // TODO Auto-generated method stub
+    	updateServiceNameMapIfNeeded();
+        return serviceNameMap.get(name);
+    }
+
+    private void updateServiceNameMapIfNeeded() {
+        if (serviceNameMap.size() != storageServices.size()) {
+            // Size differ: a service has been injected or retracted.
+            serviceNameMap.clear();
+            for (StorageService storageService : storageServices) {
+                String namedValue = getNamedAnnotationValue(storageService);
+                serviceNameMap.put(namedValue, storageService);
+            }
+        }
+    }
+
+    private String getNamedAnnotationValue(StorageService storageService) {
+        Annotation[] annotations = storageService.getClass().getAnnotations();
+        for (Annotation annotation : annotations) {
+            if (annotation instanceof Named) {
+                Named named = (Named) annotation;
+                return named.value();
+            }
+        }
+
         return null;
     }
 
