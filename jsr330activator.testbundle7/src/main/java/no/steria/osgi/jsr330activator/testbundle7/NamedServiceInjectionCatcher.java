c06792235d74053139c8c4f75eb53ac2a1c6d6ca
Steinar Bang <steb@soprasteria.com>
2015-05-21 21:33:17 +0200

    Changed service interface NamedServiceInjectionCatcher to have methods more useful for testing.

--- jsr330activator.testbundle7/src/main/java/no/steria/osgi/jsr330activator/testbundle7/NamedServiceInjectionCatcher.java
+++ jsr330activator.testbundle7/src/main/java/no/steria/osgi/jsr330activator/testbundle7/NamedServiceInjectionCatcher.java
@@ -1,5 +1,10 @@
 package no.steria.osgi.jsr330activator.testbundle7;
 
+import java.util.Collection;
+
+import no.steria.osgi.jsr330activator.testbundle8.StorageService;
+
 public interface NamedServiceInjectionCatcher {
-    public String getMessage();
+    public Collection<String> getInjectedStorageServiceNames();
+    public StorageService getStorageService(String name);
 }
