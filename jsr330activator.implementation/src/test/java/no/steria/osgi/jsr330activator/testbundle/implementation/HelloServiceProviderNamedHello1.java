cecc6e87ae68b088a278305adb766c2a036d1d79
Steinar Bang <steb@soprasteria.com>
2015-05-23 17:14:28 +0200

    Added the @Named Provider<HelloService> "hello1" for use in unit tests.

--- jsr330activator.implementation/src/test/java/no/steria/osgi/jsr330activator/testbundle/implementation/HelloServiceProviderNamedHello1.java
+++ jsr330activator.implementation/src/test/java/no/steria/osgi/jsr330activator/testbundle/implementation/HelloServiceProviderNamedHello1.java
@@ -1,10 +1,22 @@
 package no.steria.osgi.jsr330activator.testbundle.implementation;
 
+import javax.inject.Named;
 import javax.inject.Provider;
 
 import no.steria.osgi.jsr330activator.testbundle.HelloService;
 
-public class HelloServiceProvider implements Provider<HelloService> {
+/***
+ * A named provider for {@link HelloService} for use in unit tests.
+ *
+ * @author Steinar Bang
+ *
+ */
+@Named("hello1")
+public class HelloServiceProviderNamedHello1 implements Provider<HelloService>, HelloService {
+
+    public String getMessage() {
+        return "Hello1 says hi!";
+    }
 
     public HelloService get() {
         return new HelloServiceImplementation();
