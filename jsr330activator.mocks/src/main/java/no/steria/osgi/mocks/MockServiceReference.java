49852cdba0752a4975ef4c15c84b32e9fb22be94
Steinar Bang <steb@soprasteria.com>
2015-05-27 00:48:26 +0200

    Implemented the property accessor methods in MockServiceReference.

--- jsr330activator.mocks/src/main/java/no/steria/osgi/mocks/MockServiceReference.java
+++ jsr330activator.mocks/src/main/java/no/steria/osgi/mocks/MockServiceReference.java
@@ -1,12 +1,35 @@
 package no.steria.osgi.mocks;
 
+import java.util.Collections;
+import java.util.Dictionary;
+
 import org.osgi.framework.Bundle;
 
 public class MockServiceReference<S> extends MockServiceReferenceBase<S> {
     Bundle bundle;
+    private Dictionary<String, ?> properties;
 
-    public MockServiceReference(Bundle bundle2) {
-        this.bundle = bundle2;
+    public MockServiceReference(Bundle bundle, Dictionary<String, ?> properties) {
+        this.bundle = bundle;
+        this.properties = properties;
+    }
+
+    @Override
+    public Object getProperty(String key) {
+    	if (null != properties) {
+            return properties.get(key);
+    	}
+
+    	return null;
+    }
+
+    @Override
+    public String[] getPropertyKeys() {
+        if (properties != null) {
+            return Collections.list(properties.keys()).toArray(new String[1]);
+        }
+
+        return new String[0];
     }
 
     @Override
