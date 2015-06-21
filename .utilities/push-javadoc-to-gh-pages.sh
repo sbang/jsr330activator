d30fb4e267097182cc66b83248dd9f10e897af38
Steinar Bang <steb@soprasteria.com>
2015-06-21 18:11:39 +0200

    Revert "travis debug."
    This reverts commit 2d58fbe8f79f7ceb888a0109a11762f2e2edf38e.

--- .utilities/push-javadoc-to-gh-pages.sh
+++ .utilities/push-javadoc-to-gh-pages.sh
@@ -4,21 +4,19 @@ if [ "$TRAVIS_REPO_SLUG" == "sbang/jsr330activator" ] && [ "$TRAVIS_PULL_REQUEST
 
   echo -e "Publishing javadoc...\n"
 
-  ls -al jsr330activator.implementation/target/apidocs
   cp -R jsr330activator.implementation/target/apidocs $HOME/javadoc-latest
-  ls -al $HOME/javadoc-latest
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/sbang/jsr330activator gh-pages
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/sbang/jsr330activator gh-pages > /dev/null
 
   cd gh-pages
   git rm -rf ./javadoc
   cp -Rf $HOME/javadoc-latest ./javadoc
   git add -f .
   git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages
+  git push -fq origin gh-pages > /dev/null
 
   echo -e "Published Javadoc to gh-pages.\n"
   
