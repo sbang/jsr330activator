25b0159d97158893823c857b5466ca60906e0ef3
Steinar Bang <steb@soprasteria.com>
2017-05-03 21:05:09 +0200

    Copy files verbatim to preserve history.

---
+++ jsr330activator.implementation/src/test/java/no/steria/osgi/jsr330activator/testbundle/implementation/HelloServiceProviderNamedAndPropertyHello2.java
@@ -0,0 +1,25 @@
+package no.steria.osgi.jsr330activator.testbundle.implementation;
+
+import javax.inject.Named;
+import javax.inject.Provider;
+
+import no.steria.osgi.jsr330activator.testbundle.HelloService;
+
+/***
+ * A named provider for {@link HelloService} for use in unit tests.
+ *
+ * @author Steinar Bang
+ *
+ */
+@Named("hello2")
+public class HelloServiceProviderNamedHello2 implements Provider<HelloService>, HelloService {
+
+    public String getMessage() {
+        return "Hello2 says hi!";
+    }
+
+    public HelloService get() {
+        return this;
+    }
+
+}
